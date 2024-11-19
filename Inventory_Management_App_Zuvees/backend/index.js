2ea5f4dada0b4297867b854b3938874efdfc96b4
kanhaiyak23 <kanhaiyakumarsingh086@gmail.com>
2024-11-20 01:09:23 +0530

    allow-origin

--- Inventory_Management_App_Zuvees/backend/index.js
+++ Inventory_Management_App_Zuvees/backend/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','https://zuvees-red.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
